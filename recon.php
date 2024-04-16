cee104dc14c7aaee124b49f11747af7c662f8b6f
Erixk12 <erick.lopez6523@alumnos.udg.mx>
2024-04-16 08:55:53 -0600

    [Olvidar Contraseña] (fix) -> Arreglo de errores

--- recon.php
+++ recon.php
@@ -1,16 +1,24 @@
 <?php
+ob_start();
 include("conexion.php");
 
+
+ 
 // Verificar la conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-// Recibir los datos del formulario
-$correoelectronico = $_POST["email"];
+// Recibir los datos del formulario y sanitizarlos
+$correoelectronico = isset($_POST["email"]) ? $conexion->real_escape_string($_POST["email"]) : '';
 
 // Consulta para verificar si el usuario ya existe
 $consulta = mysqli_query($conexion, "SELECT * FROM Usuarios WHERE correoelectronico = '$correoelectronico'");
+
+if (mysqli_num_rows($consulta) == 0) {
+    header("location:index.php?valor=2");
+    exit; // Terminar la ejecución del script después de redirigir
+} 
 
 while($fila = mysqli_fetch_array($consulta)){
     $correoelectronico = $fila['correoelectronico'];
@@ -21,23 +29,41 @@ while($fila = mysqli_fetch_array($consulta)){
 if ($correoelectronico==NULL){header("location:index.php?valor=2");} 
 else {
     $permited_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $numale = substr(str_shuffle($permited_chars), 0, 10);
-    $enlace = "https://fansdejennirivera2007.000webhostapp.com/ForgotPassword.html?cod='$codigo'";
+    $codigo = substr(str_shuffle($permited_chars), 0, 10);
+    $enlace = "https://fansdejennirivera2007.000webhostapp.com/ForgotPassword.html?cod=$codigo";
     echo $enlace;
-    $consulta = mysqli_query($conexion, "insert into solcon (idu, codigo)");
+    $consulta = mysqli_query($conexion, "insert into solcon (iduser, codigo, email) VALUES ('$id', '$codigo', '$correoelectronico')");
 
-    
-    $para      = '$cor';
-    $titulo    = 'Recuperar contraseña';
-    $mensaje   = 'Recuperar';
-    $cabeceras = 'From: chido@empresa.com' . "\r\n" .
-        'Reply-To: webmaster@empresa.com' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    
-    mail($para, $titulo, $mensaje, $cabeceras);
+    header("location:login.html?cod=$codigo");  
     
 }
 
 // Cerrar la conexión
 $conexion->close();
+
+ob_end_flush();
+
+
+
+
+
+
+
+
+
+
+
+// $para      = '$cor';
+    // $titulo    = 'Recuperar contraseña';
+    // $mensaje   = 'Recuperar';
+    // $cabeceras = 'From: empreverest@gmail.com' . "\r\n" .
+    //     'Reply-To: webmaster@empresa.com' . "\r\n" .
+    //     'X-Mailer: PHP/' . phpversion();
+    
+    // mail($para, $titulo, $mensaje, $cabeceras);
+
+
 ?>
+
+
+
