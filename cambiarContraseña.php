9506b78e62584739ab27cce2f58fcbff474e6229
Isaac Martínez <98341452+Martinezisaac@users.noreply.github.com>
2024-04-23 08:34:36 -0600

    [BD] (Cambiar contraseña) -> Se agregó una validación para el código de cambiar contraseña

--- cambiarContraseña.php
+++ cambiarContraseña.php
@@ -13,6 +13,7 @@ $consulta = mysqli_query($conexion, "SELECT * FROM cambiarContraseña WHERE codi
 // Buscar el ID y el Correo electronico de la base de datos
 while($fila = mysqli_fetch_array($consulta)){ //Encontrar variables de la tabla 
     $id = $fila['iduser']; //Obtener el ID del usuario del codigo ingresado 
+    $codigoBD = $fila['codigo'];
 }
 
 // Verificar si las nuevas contraseñas no están vacías
@@ -29,9 +30,11 @@ if (!empty($_POST["cambiarContraseña"]) && !empty($_POST["confirmarContraseña"
     $filaContraseña = mysqli_fetch_assoc($consultaContraseña);
     $contraseñaActual = $filaContraseña['contraseña']; //Obtener la contraseña actual (la contraseña ya esta cifrada en Hash1)
 
-    // Verificar si la nueva contraseña coincide con la contraseña actual
-    if($confirmarContraseñaHash === $contraseñaActual && $cambiarContraseñaHash === $contraseñaActual) { // Validar si la nueva contraseña es igual a la contraseña anterior
-        header("location: login.html?login=success&cod=$codigo&login=fail&error=same_passwords"); 
+    if($codigo != $codigoBD) { // Verificar si el codigo ingresado es igual al de la base de datos
+        header("location: login.html?login=success&cod=$codigo&login=fail&error=wrong_passwords");  //Mostrar error
+    } // Verificar si la nueva contraseña coincide con la contraseña actual
+    else if($confirmarContraseñaHash === $contraseñaActual && $cambiarContraseñaHash === $contraseñaActual) { // Validar si la nueva contraseña es igual a la contraseña anterior
+        header("location: login.html?&login=fail&error=same_passwords&cod=$codigoBD"); 
     } 
     else if ($confirmarContraseñaHash === $cambiarContraseñaHash) { 
         // Actualizar la contraseña en la base de datos
@@ -49,7 +52,7 @@ if (!empty($_POST["cambiarContraseña"]) && !empty($_POST["confirmarContraseña"
     }
     else {
         //header("Location: login.html?login=fail&error=wrong_passwords"); // Mostrar error
-        header("location: login.html?login=success&cod=$codigo&login=fail&error=wrong_passwords");  //Mostrar error
+        header("location: login.html?&login=fail&error=same_passwords&cod=$codigoBD");   //Mostrar error
     }
 }
 
