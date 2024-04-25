16e6232fc450ab3ef4fda5b2c7585fd34e1d6f40
Isaac Martínez <98341452+Martinezisaac@users.noreply.github.com>
2024-04-25 13:26:51 -0600

    Update val_correoElectronico.php

--- val_correoElectronico.php
+++ val_correoElectronico.php
@@ -1,5 +1,6 @@
 <?php
 ob_start();
+session_start(); //Iniciar una sesión
 include("conexion.php");
 
 // Verificar la conexión
@@ -10,40 +11,38 @@ if ($conexion->connect_error) {
 // Recibir los datos del formulario y sanitizarlos
 $correoelectronico = isset($_POST["email"]) ? $conexion->real_escape_string($_POST["email"]) : '';
 
-// Verificar si el correo electrónico está vacío
-if ($correoelectronico === '') {
+if ($correoelectronico === '') { // Verificar si el correo electrónico está vacío
     // Redirigir al usuario a alguna página de error o mostrar un mensaje adecuado
     header("location: login.html?login=fail&error=empty_email");
-    exit; // Terminar la ejecución del script después de redirigir
+    exit(); // Terminar la ejecución del script después de redirigir
 }
 
 // Consulta para verificar si el usuario ya existe
 $consulta = mysqli_query($conexion, "SELECT * FROM Usuarios WHERE correoelectronico = '$correoelectronico'");
 
-// Validar si existe un correo en la base de datos 
-if (mysqli_num_rows($consulta) == 0) {
+if (mysqli_num_rows($consulta) == 0) { // Validar si existe un correo en la base de datos 
     header("location:login.html?login=fail&error=empty_email");
-    exit; // Terminar la ejecución del script después de redirigir
+    exit(); // Terminar la ejecución del script después de redirigir
 } 
 
-// Buscar el ID y el Correo electronico de la base de datos
-while($fila = mysqli_fetch_array($consulta)){
+while($fila = mysqli_fetch_array($consulta)){ // Buscar el ID y el Correo electronico de la base de datos
     $correoelectronico = $fila['correoelectronico'];
     $id = $fila['id'];
 }
 
-// Validar si el correo electronico es diferente de null, es decir si existe en la base de datos
-if ($correoelectronico == NULL){
+if ($correoelectronico == NULL){ // Validar si el correo electronico es diferente de null, es decir si existe en la base de datos
     header("location:login.html?login=fail&error=empty_email");
-    exit; // Terminar la ejecución del script después de redirigir
+    exit(); // Terminar la ejecución del script después de redirigir
 } else {
     $permited_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $codigo = substr(str_shuffle($permited_chars), 0, 10);
+    $_SESSION['codigo'] = $codigo; //Guardar codigo en una sesión
 
     // Insertar datos en la tabla cambiarContraseña
     mysqli_query($conexion, "INSERT INTO cambiarContraseña (iduser, codigo, email) VALUES ('$id', '$codigo', '$correoelectronico')");
     // Redirigir a donde necesites después de guardar el código en la base de datos
-    header("location:login.html?login=success&cod=$codigo"); //Mostrar error
+    header("location:login.html?login=error&cod=error"); //Mostrar error
+    exit();
 }
 
 ob_end_flush();
