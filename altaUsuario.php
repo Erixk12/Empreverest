636b51070bfc60edc4c2df0303574a4e98cd0431
Erixk12 <erick.lopez6523@alumnos.udg.mx>
2024-05-02 05:54:52 -0600

    [Registrarse] (feat) -> Validaciones de datos

--- altaUsuario.php
+++ altaUsuario.php
@@ -15,23 +15,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Obtiene los datos del formulario
     $correoElectronico = $_POST['email'];
-    $contrasena = sha1($_POST['password']);
+    $contrasena = $_POST['password'];
     $estado = true; // Hasta que el usuario no complete los demas datos no se pondra true
     $fecha = date("Y-m-d H:i:s"); // Obtiene la fecha y hora actual
     
-    $permited_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $codigo = substr(str_shuffle($permited_chars), 0, 10);
-    $_SESSION['codigo'] = $codigo;
+    // Verificar si el correo electrónico contiene ".udg.mx" como dominio
+    if (!preg_match("/@([a-zA-Z0-9-]+)\.udg\.mx$/", $correoElectronico)) {
+        header("Location: Registrar_Usuario.html?registration=fail&error=invalid_email");
+        exit(); // Terminar la ejecución del script después de redirigir
+    }
 
-    // Prepara la consulta SQL para insertar los datos en la tabla Usuarios
-    $sql = "INSERT INTO Usuarios (correoelectronico, contraseña, estado, fecha, codigo) VALUES ('$correoElectronico', '$contrasena', '$estado', '$fecha', '$codigo')";
+    // Consulta para verificar si el usuario ya existe
+    $consulta = mysqli_query($conexion, "SELECT * FROM Usuarios WHERE correoelectronico = '$correoElectronico'");
 
-    // Ejecuta la consulta
-    if (mysqli_query($conexion, $sql)) {
-        header("Location: Registrarse.html?cod=$codigo");
-        exit(); // Asegura que el script se detenga después de la redirección
+    if (mysqli_num_rows($consulta) > 0) {
+        // El correo electrónico ya existe en la base de datos
+        header("location:Registrar_Usuario.html?registration=fail&error=existing_email");
+        exit(); // Terminar la ejecución del script después de redirigir
+    } elseif (strlen($contrasena) < 8) {
+        // La contraseña es insegura
+        header("location:Registrar_Usuario.html?registration=fail&error=insecure_password");
+        exit(); // Terminar la ejecución del script después de redirigir
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+        $contrasena = sha1($_POST['password']);
+        $permited_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $codigo = substr(str_shuffle($permited_chars), 0, 10);
+        $_SESSION['codigo'] = $codigo;
+    
+        // Prepara la consulta SQL para insertar los datos en la tabla Usuarios
+        $sql = "INSERT INTO Usuarios (correoelectronico, contraseña, estado, fecha, codigo) VALUES ('$correoElectronico', '$contrasena', '$estado', '$fecha', '$codigo')";
+    
+        // Ejecuta la consulta
+        if (mysqli_query($conexion, $sql)) {
+            header("Location: Registrarse.html?cod=$codigo");
+            exit(); 
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+        }
+
     }
 
     // Cierra la conexión
--- registrarUsuario.php
+++ registrarUsuario.php
@@ -30,24 +30,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mysqli_stmt_execute($stmt);
     mysqli_stmt_store_result($stmt);
 
-    // Verificar si se encontró un usuario con el código dado
-    if (mysqli_stmt_num_rows($stmt) > 0) {
-        mysqli_stmt_bind_result($stmt, $iduser);
-        mysqli_stmt_fetch($stmt);
+    // Consulta para verificar si el usuario ya existe
+    $consulta = mysqli_query($conexion, "SELECT * FROM personas WHERE codigo_alumno = '$codigoAlumno'");
 
-        // Consulta SQL para insertar en la tabla personas
-        $sql = "INSERT INTO personas (iduser, nombre, apellido, genero, tipocuenta, codigo_alumno, centro) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = mysqli_prepare($conexion, $sql);
-        mysqli_stmt_bind_param($stmt, "issssss", $iduser, $nombre, $apellidos, $genero, $tipoCuenta, $codigoAlumno, $centroUniversitario);
-
-        if (mysqli_stmt_execute($stmt)) {
-            echo "Éxito";
+    if (!preg_match("/^[a-zA-Z\s]+$/", $nombre) || !preg_match("/^[a-zA-Z\s]+$/", $apellidos)) {
+        // Verificar si el nombre y el apellido contienen solo letras y espacios
+        header("location:Registrarse.html?registration=fail&error=invalid_name_or_lastname&cod=$codigo");
+        exit(); // Terminar la ejecución del script después de redirigir
+    }   
+    elseif (strlen($nombre) < 3) {
+        // Verificar que el nombre tenga al menos 3 caracteres
+        header("location:Registrarse.html?registration=fail&error=short_name&cod=$codigo");
+        exit(); // Terminar la ejecución del script después de redirigir
+    }
+    elseif (strlen($apellidos) < 3) {
+        // Verificar que el apellido tenga al menos 3 caracteres
+        header("location:Registrarse.html?registration=fail&error=error=short_name&cod=$codigo");
+        exit(); // Terminar la ejecución del script después de redirigir
+    }
+    elseif (mysqli_num_rows($consulta) > 0) {
+        // El codigo de alumno ya existe en la base de datos
+        header("location:Registrarse.html?registration=fail&error=existing_studentcode&cod=$codigo");
+        exit(); // Terminar la ejecución del script después de redirigir
+    }        
+    elseif (!preg_match("/^\d{9}$/", $codigoAlumno)) {
+        // Verificar que el código tenga exactamente 9 caracteres y que sean todos números
+        header("location:Registrarse.html?registration=fail&error=invalid_student_code&cod=$codigo");
+        exit(); // Terminar la ejecución del script después de redirigir
+    }    
+    else{
+        // Verificar si se encontró un usuario con el código dado
+        if (mysqli_stmt_num_rows($stmt) > 0) {
+            mysqli_stmt_bind_result($stmt, $iduser);
+            mysqli_stmt_fetch($stmt);
+    
+            // Consulta SQL para insertar en la tabla personas
+            $sql = "INSERT INTO personas (iduser, nombre, apellido, genero, tipocuenta, codigo_alumno, centro) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = mysqli_prepare($conexion, $sql);
+            mysqli_stmt_bind_param($stmt, "issssss", $iduser, $nombre, $apellidos, $genero, $tipoCuenta, $codigoAlumno, $centroUniversitario);
+    
+            if (mysqli_stmt_execute($stmt)) {
+                echo "Éxito";
+            } else {
+                echo "Error al insertar datos: " . mysqli_error($conexion);
+            }
         } else {
-            echo "Error al insertar datos: " . mysqli_error($conexion);
+            echo "No se encontró ningún usuario con el código proporcionado.";
+            echo $codigo;
         }
-    } else {
-        echo "No se encontró ningún usuario con el código proporcionado.";
-        echo $codigo;
     }
 
     // Cierra la conexión
